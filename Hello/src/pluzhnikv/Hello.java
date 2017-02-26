3945517d3f7c1d35a670d25fd92eeb7b78a83e17
victor <victor@victor-1215T>
2017-02-26 20:01:04 +0300

    version 2

--- Hello/src/pluzhnikv/Hello.java
+++ Hello/src/pluzhnikv/Hello.java
@@ -3,5 +3,6 @@ package pluzhnikv;
 public class Hello {
 	public static void main(String[] args) {
         System.out.println("Hello World from Victor!");
+        System.out.println("New version 2!");
     }
 }
